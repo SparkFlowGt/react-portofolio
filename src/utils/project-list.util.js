8df68aa14c6d0cba8b7210dc615111042956cf1d
SparkFlowGt <scinteiestefan@gmail.com>
2023-05-05 12:45:06 +0300

    added ticket project

--- src/utils/project-list.util.js
+++ src/utils/project-list.util.js
@@ -1,6 +1,7 @@
 import corona from '.././assets/coronaClothing.PNG';
 import crownSvg from '.././assets/crown.svg';
 import robo from '.././assets/robofriends.PNG';
+import ticket from '.././assets/the_precious_tickets.PNG'
 
 export const projectList=[
     {
@@ -18,5 +19,13 @@ export const projectList=[
         domainLink:"https://sparkflowgt.github.io/robofriends/",
         description:"This React site is a simple but powerful tool that allows users to search through a series of objects for name and email addresses. With the use of JavaScript and React, I was able to create a responsive and user-friendly search functionality that makes it easy for users to find the information they need."
     },
+    {
+        name:'The PreciousTickets',
+        image: ticket ,
+        githubLink:"https://github.com/SparkFlowGt/the_precious_events",
+        domainLink:"https://lucky-bombolone-49b552.netlify.app/",
+        description:"This project was a ticket website built with React, featuring a modern and responsive design. The website included a countdown timer, slider, and side drawer to enhance the user experience and create a sense of urgency for upcoming events. The website was optimized for all devices and was designed with a clean and simple look. Overall, this project provided a modern and user-friendly ticket-selling platform that met the needs of the business."
+    },
  
 ]
+
